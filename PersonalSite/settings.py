4337079eff6db9c8b5d61a6eb0de37ec40080d05
wh01683 <wh01683@georgiasouthern.edu>
2016-05-12 11:43:55 -0400

    Maybe css will work on deployment.

--- PersonalSite/settings.py
+++ PersonalSite/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATIC_ROOT = "/var/http:/roberthowerton-env.us-east-1.elasticbeanstalk.com/static/"
