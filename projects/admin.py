04bc85d9066b460d35f1ddaa9d5e498726e12b4e
wh01683 <wh01683@georgiasouthern.edu>
2016-05-13 08:58:57 -0400

    Registered project models on admin site.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from projects.models import Project, Skill, ProjectSkill
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Skill)
+admin.site.register(ProjectSkill)
